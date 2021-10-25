5ad335acdb76f30e29b0a73e319f17b3916c1e17
Luis Osorio <losorio1986@gmail.com>
2021-10-25 20:16:40 -0300

    adicionado o cloudinary e o active storage ao projeto

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Action", "Drama", "Thriller", "Comedy", "Horror"],
-    typeSpeed: 50,
+    typeSpeed: 150,
     loop: true
   });
 }
